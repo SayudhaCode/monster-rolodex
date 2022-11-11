2b951ad599572cc7ae5c707cfe5fcc0e93e54dc5
sayudha <mazaya.studio@outlook.com>
2022-11-11 07:42:11 +0700

    searching and filtering

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -13,16 +13,33 @@ class App extends Component {
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
-		.then(users => this.setState(
-							() => {return { monsters: users };},
-							() => {console.log(this.state);},
-						),
-				);
+		.then(
+			users => this.setState(
+	() => {return { monsters: users };},
+() => {console.log(this.state);},
+			),
+		);
 	}
 
 	render() {
 		return (
 				<div className="App">
+					<input
+							className="search-box"
+							type="search"
+							placeholder="search monsters"
+							onChange={ (event) => {
+								const searchString = event.target.value.toLocaleLowerCase();
+								const filteredMonsters = this.state.monsters.filter(
+										(monster) => {
+											return monster.name.toLocaleLowerCase().includes(searchString);
+										});
+
+								this.setState(() => {
+									return { monsters: filteredMonsters };
+								});
+							} }
+					/>
 					{
 						this.state.monsters.map((monster) => {
 							return (
