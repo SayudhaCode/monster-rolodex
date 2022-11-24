b37bd8e6132ca51fdcf0bc57904ebe9709cf90ef
sayudha <mazaya.studio@outlook.com>
2022-11-24 07:49:31 +0700

    using useEffect and change all component into functional component

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,14 +1,28 @@
-// import CardList from './components/card-lists/card-list.component';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+import CardList from './components/card-lists/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  const onSearchChange =(event)=> {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
@@ -21,6 +35,7 @@ const App = () => {
         onChangeHandler={ onSearchChange }
         placeholder="search monsters"
       />
+      <CardList monsters={ filteredMonsters }/>
     </div>
   );
 };
